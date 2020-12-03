fef46865dad3e76ea9e835ce57d06a7a79dbdd21
Israel Ferreira <israelferreira.github@gmail.com>
2020-12-02 23:37:40 -0300

    Validation was improved with a better exception handler.

--- src/main/java/academy/devdojo/springboot2/handler/RestExceptionHandler.java
+++ src/main/java/academy/devdojo/springboot2/handler/RestExceptionHandler.java
@@ -1,14 +1,19 @@
 package academy.devdojo.springboot2.handler;
 
 import java.time.LocalDateTime;
+import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
 import academy.devdojo.springboot2.exception.BadRequestException;
 import academy.devdojo.springboot2.exception.BadRequestExceptionDetails;
+import academy.devdojo.springboot2.exception.ValidationExceptionDetails;
 
 @ControllerAdvice
 public class RestExceptionHandler {
@@ -25,4 +30,24 @@ public class RestExceptionHandler {
 						.build(), HttpStatus.BAD_REQUEST);
 	}
 	
+
+	@ExceptionHandler(MethodArgumentNotValidException.class)
+	public ResponseEntity<ValidationExceptionDetails> handlerMethodArgumentNotValidException(
+			MethodArgumentNotValidException notValidException) {
+		
+		List<FieldError> fieldErrors = notValidException.getBindingResult().getFieldErrors();
+		String fields = fieldErrors.stream().map(FieldError::getField).collect(Collectors.joining(", "));
+		String fieldsMessage = fieldErrors.stream().map(FieldError::getDefaultMessage).collect(Collectors.joining(", "));
+			
+		return new ResponseEntity<>(
+				ValidationExceptionDetails.builder()
+						.timestamp(LocalDateTime.now())
+						.status(HttpStatus.BAD_REQUEST.value())
+						.title("Bad Request Exception, Invalid fields")
+						.details("Check the field(s) error")
+						.developerMessage(notValidException.getClass().getName())
+						.fields(fields)
+						.fieldsMessage(fieldsMessage)
+						.build(), HttpStatus.BAD_REQUEST);
+	}
 }
