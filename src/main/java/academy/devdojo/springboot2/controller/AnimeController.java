059298746a1905be50817cfd1db6f2b73050319f
Israel Ferreira <israelferreira.github@gmail.com>
2020-12-09 23:57:20 -0300

    Prometheus and Grafana added to the project

--- src/main/java/academy/devdojo/springboot2/controller/AnimeController.java
+++ src/main/java/academy/devdojo/springboot2/controller/AnimeController.java
@@ -9,8 +9,6 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -25,6 +23,9 @@ import academy.devdojo.springboot2.domain.Anime;
 import academy.devdojo.springboot2.requests.AnimePostRequestBody;
 import academy.devdojo.springboot2.requests.AnimePutRequestBody;
 import academy.devdojo.springboot2.service.AnimeService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import lombok.RequiredArgsConstructor;
 
 @RestController
@@ -35,43 +36,62 @@ public class AnimeController {
 	private final AnimeService animeService;
 	
 	@GetMapping
+	@Operation(summary = "List all animes paginated", description = "The default size is 20, use the parameter size to change the default value",
+		    tags = {"anime"})
 	public ResponseEntity<Page<Anime>> list(@ParameterObject Pageable pageable) {
 		return ResponseEntity.ok(animeService.listAll(pageable));
 	}
 	
 	@GetMapping(path = "/all")
+	@Operation(summary = "List all animes without pagination", tags = {"anime"})
 	public ResponseEntity<List<Anime>> listAll() {
 		return ResponseEntity.ok(animeService.listAllNonPageable());
 	}
 	
 	@GetMapping(path = "/{id}")
+	@Operation(summary = "Find an anime by id", description = "The id parameter is a number", tags = {"anime"})
+	@ApiResponses(value = {
+			@ApiResponse(responseCode = "200", description = "Successful Operation"),
+			@ApiResponse(responseCode = "400", description = "When Anime Does Not Exist in The Database")
+	})
 	public ResponseEntity<Anime> findById(@PathVariable Long id) {
 		return ResponseEntity.ok(animeService.findByIdOrThrowBadRequestException(id));
 	}
-	
-	@GetMapping(path = "by-id/{id}")
-	public ResponseEntity<Anime> findByIdAuthenticationPrincipal(
-			@PathVariable Long id, @AuthenticationPrincipal UserDetails userDetails) {
-		return ResponseEntity.ok(animeService.findByIdOrThrowBadRequestException(id));
-	}
-	
+		
 	@GetMapping(path = "/find")
+	@Operation(summary = "Search for animes by the name", tags = {"anime"})
 	public ResponseEntity<List<Anime>> findByName(@RequestParam String name) {
 		return ResponseEntity.ok(animeService.findByName(name));
 	}
 	
 	@PostMapping
+	@Operation(summary = "Save an anime in the database",
+			description = "The name is required, it cannot be empty or null", tags = {"anime"})
+	@ApiResponses(value = {
+			@ApiResponse(responseCode = "201", description = "Anime saved in database"),
+			@ApiResponse(responseCode = "400", description = "When the anime name is empty or null")
+	})
 	public ResponseEntity<Anime> save(@RequestBody @Valid AnimePostRequestBody animePostRequestBody) {
 		return new ResponseEntity<>(animeService.save(animePostRequestBody), HttpStatus.CREATED);
 	}
 	
 	@DeleteMapping(path = "/admin/{id}")
+	@Operation(summary = "Delete an anime by id", description = "The id parameter is a number", tags = {"anime"})
+	@ApiResponses(value = {
+			@ApiResponse(responseCode = "204", description = "Successful Operation"),
+			@ApiResponse(responseCode = "400", description = "Anime Does Not Exist in The Database")
+	})
 	public ResponseEntity<Void> delete(@PathVariable Long id) {
 		animeService.delete(id);
 		return new ResponseEntity<>(HttpStatus.NO_CONTENT);
 	}
 	
 	@PutMapping
+	@Operation(summary = "Replace an anime(update all fields) by id", tags = {"anime"})
+	@ApiResponses(value = {
+			@ApiResponse(responseCode = "204", description = "Successful Operation"),
+			@ApiResponse(responseCode = "400", description = "Anime Does Not Exist or Name is empty or null")
+	})
 	public ResponseEntity<Void> replace(@RequestBody @Valid AnimePutRequestBody animePutRequestBody) {
 		animeService.replace(animePutRequestBody);
 		return new ResponseEntity<>(HttpStatus.NO_CONTENT);
--- src/main/java/academy/devdojo/springboot2/requests/AnimePutRequestBody.java
+++ src/main/java/academy/devdojo/springboot2/requests/AnimePutRequestBody.java
@@ -2,6 +2,7 @@ package academy.devdojo.springboot2.requests;
 
 import javax.validation.constraints.NotEmpty;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Builder;
 import lombok.Data;
 
@@ -12,5 +13,6 @@ public class AnimePutRequestBody {
 	private Long id;
 
 	@NotEmpty(message = "The anime name cannot be empty")
+	@Schema(description = "This is the Anime's name", example = "Tensei Shittara Slime Datta Ken", required = true)
 	private String name;
 }
